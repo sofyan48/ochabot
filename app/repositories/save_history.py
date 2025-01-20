9814448175d660715fd36b4ed6fa276ba223cfab
sofyan48 <meongbego@gmail.com>
2025-01-20 22:23:21 +0700

    Remove Save history

--- app/repositories/save_history.py
+++
@@ -1,9 +0,0 @@
-from app import mysql
-
-class History(object):
-    def __init__(self):
-        self.write = mysql.write_client()
-        self.read = mysql.read_client()
-        
-    def save_history(self):
-        pass
