485124108d3066323812e432b3cfc6ed733fb7d5
sofyan48 <meongbego@gmail.com>
2025-01-25 21:59:25 +0700

    Add: Fixing cinbot from

--- bootstrap/mistral.py
+++ bootstrap/mistral.py
@@ -3,9 +3,9 @@ import os
 
 def register_mistral():
     template = """Your name is Cinbot Mistral.
-    Answer in Bahasa Indonesia
-    Jika ada pertnayaan selain context maka jawab hanya scope teknologi informasi saja jika tidak maka
-    jawab dengan, maaf kak kontek saya dari om iank terbatas.
+    Answer in Bahasa Indonesia, utamakan context om iank,
+    Jika ada pertanyaan selain context maka jawab hanya terkait teknologi saja.
+    jika pertanya selain om iank dan teknologi maka jawab saya tidak tau kak
     Histroy: {history}
     Context: {context}
     Question: {input}
--- bootstrap/openai.py
+++ bootstrap/openai.py
@@ -4,8 +4,9 @@ import os
 def register_openai() -> OpenAILLM:
     template = """Your name is Cinbot Mistral.
     Answer in Bahasa Indonesia
-    Jika ada pertnayaan selain context maka jawab hanya scope teknologi informasi saja jika tidak maka
-    jawab dengan, maaf kak kontek saya dari om iank terbatas.
+    utamakan context om iank,
+    Jika ada pertnayaan selain context maka jawab hanya terkait teknologi saja.
+    jika pertanya selain om iank dan teknologi maka jawab saya tidak tau kak
     Histroy: {history}
     Context: {context}
     Question: {input}
