3c4405b7825371dcdec9aee9a646cb6efd247686
sofyan48 <meongbego@gmail.com>
2025-01-27 18:25:01 +0700

    Fixing: Model Socket Client

--- app/repositories/socket_client.py
+++ app/repositories/socket_client.py
@@ -1,43 +1,30 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
-from sqlalchemy import text, bindparam, insert
-from typing import Optional, List
-from sqlalchemy import select, and_, Table, Column, Integer, String, MetaData, DateTime
-
-metadata = MetaData()
-
-client_sockets = Table(
-    "client_sockets",
-    metadata,
-    Column("id", Integer),
-    Column("name", String),
-    Column("secret", String),
-    Column("created_at", DateTime),
-    Column("updated_at", DateTime),
-)
-
-class ClientSocket(object):
-    def __init__(self, engine: AsyncEngine):
-        self.engine = engine
-        
-    async def fetch(self, id: int) -> Optional[dict]:
-        query = (
-            select(client_sockets).where(client_sockets.c.id == id)
-        )            
-        async with AsyncSession(self.engine) as session:
-            try:
-                result = await session.execute(query)
-                rows = result.fetchall()
-                return [dict(row._mapping) for row in rows] if rows else []
-            except Exception as e:
-                raise e
-            
-    async def insert(self, name: str, secret: str) -> int:  
-        query = insert(client_sockets).values(name=name, secret=secret)  
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession  
+from sqlalchemy import select, insert  
+from typing import Optional  
+from datetime import datetime
+from app.entitity.client_socket import ClientSocket
+  
+class ClientSocketService:  
+    def __init__(self, engine: AsyncEngine):  
+        self.engine = engine  
+          
+    async def fetch(self, id: int) -> Optional[dict]:  
+        query = select(ClientSocket).where(ClientSocket.id == id)  
         async with AsyncSession(self.engine) as session:  
             try:  
                 result = await session.execute(query)  
-                await session.commit()
-                return result.inserted_primary_key[0]
+                row = result.scalar_one_or_none()
+                return dict(row) if row else None
             except Exception as e:  
-                await session.rollback()
                 raise e  
+              
+    async def insert(self, name: str, secret: str) -> int:    
+        query = insert(ClientSocket).values(name=name, secret=secret, created_at=datetime.now(), updated_at=datetime.now())    
+        async with AsyncSession(self.engine) as session:    
+            try:    
+                result = await session.execute(query)    
+                await session.commit()  
+                return result.inserted_primary_key[0]  
+            except Exception as e:    
+                await session.rollback()  
+                raise e    
