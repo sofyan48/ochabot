8fbbaff152c4abf12d74a61493d873a7539a3a70
sofyan48 <meongbego@gmail.com>
2025-02-01 22:42:34 +0700

    Setup Config Combination redis to Postgree

--- app/library/setup.py
+++ app/library/setup.py
@@ -1,7 +1,7 @@
 from app.repositories import redis
 from app.repositories.setup import SetupConfig
 from pkg.redis import Redis
-from app.library import setup_repo
+from app.library import repo_config
 
 class SetupConfigLibrary(object):
     _instance = None  # Class variable to hold the singleton instance
@@ -12,14 +12,20 @@ class SetupConfigLibrary(object):
         if cls._instance is None:
             cls._instance = super(SetupConfigLibrary, cls).__new__(cls)
             cls._instance_redis = redis
-            cls._instance_repo = setup_repo
+            cls._instance_repo = repo_config
         return cls._instance
     
     @classmethod
     async def save_all(cls):
-        data_config = await cls._instance_repo.list_key()
+        try:
+            data_config = await cls._instance_repo.list_key()
+        except Exception as e:
+            raise e
         for i in data_config:
-            await cls._instance_redis.set(i.get('keys'), i.get('value'))
+            try:
+                await cls._instance_redis.set(i.get('key'), i.get('value'))
+            except Exception as e:
+                raise e
     
     @classmethod
     async def get_all_config(cls):
@@ -29,4 +35,5 @@ class SetupConfigLibrary(object):
             key_value_pairs = {key: await cls._instance_redis.get(key) for key in keys}
             return key_value_pairs
         except Exception as e:
+            print("ERRR:", e)
             raise e
--- app/ucase/setup/llm_setup.py
+++ app/ucase/setup/llm_setup.py
@@ -2,7 +2,7 @@ from app.presentation import request
 from fastapi.security import HTTPBasicCredentials
 from fastapi import Depends, HTTPException, status
 from app.appctx import IGetResponseBase, response
-from app.ucase.setup import router, auth, logger, setup_repo
+from app.ucase.setup import router, auth, logger, setup_repo, setup_library
 from app.ucase import BasicAuth
 
 @router.post("/setup/llm", tags=["setup"], operation_id="setup_llm_insert") 
@@ -13,12 +13,14 @@ async def setup_llm_insert(payload: request.RequestLLMSetup,
     try:
         await setup_repo.model(payload.model)
         await setup_repo.platform(payload.platform)
+        await setup_library.save_all()
     except Exception as e:
         logger.error("Error saving llm config", {
             "error": str(e),
         })
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+    
     return response(
         message="LLM config saved successfully",
         data=payload.model_dump()
