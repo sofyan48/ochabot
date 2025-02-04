7a6afb72f3eaef3649854d3ae03092589b1dd66d
sofyan48 <meongbego@gmail.com>
2025-02-04 15:41:57 +0700

    Remove: debugging

--- main.py
+++ main.py
@@ -15,15 +15,14 @@ if __name__ == '__main__':
     parser.add_argument("command", help="Perintah yang ingin Anda jalankan")
     args = parser.parse_args()
     if args.command == "serve":
-        from app import app
-        from config.multiprocessing import UvicornWorkerRecommender
-        worker = UvicornWorkerRecommender().recommend_workers()
+        # from config.multiprocessing import UvicornWorkerRecommender
+        # worker = UvicornWorkerRecommender().recommend_workers()
         if utils.environment_transform() != "loc":
             uvicorn.run(
                 app="app:app",
                 host=os.getenv("APP_HOST", "0.0.0.0"),
                 port=int(os.getenv("APP_PORT", "8080")),
-                workers=worker,
+                # workers=worker,
                 reload=False,
                 log_config=logging_config
             )
