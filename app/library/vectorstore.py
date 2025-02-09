3c2988eafeeaa392fa8992ca4307a44dd00f4407
sofyan48 <meongbego@gmail.com>
2025-02-10 05:31:35 +0700

    Fixing vectorstore library

--- app/library/vectorstore.py
+++ app/library/vectorstore.py
@@ -6,18 +6,23 @@ class Vectorstores(object):
     def __init__(self):
         pass
 
-    def configure(self, vectorestore = None) -> ChromaDB | ElasticsearcVector:
-        
-        if vectorestore is None:
-            if ChromaDB._chroma is None:
+    def chroma_client(self):
+        if ChromaDB._chroma is None:
                 logger.error("ChromaDB not connect, please import or declare on init")
                 raise "Chroma not connect"
-            return ChromaDB
+        return ChromaDB
+    
+    def elastic_client(self):
+        if ElasticsearcVector._elastic is None:
+            logger.error("Elasticsearch not connect, please import or declare on init")
+            raise "Elasticsearch not connect"
+        return ElasticsearcVector
+
+    def configure(self, vectorestore = None) -> ChromaDB | ElasticsearcVector:
+        if vectorestore is None:
+            return self.chroma_client()
         
         if vectorestore=="elastic":
-            if ElasticsearcVector._elastic is None:
-                logger.error("Elasticsearch not connect, please import or declare on init")
-                raise "Elasticsearch not connect"
-            return ElasticsearcVector
+            return self.elastic_client()
         else:
-            return ChromaDB
+            return self.chroma_client()
